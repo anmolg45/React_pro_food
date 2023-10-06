c046572ee27a615bcbc7f1250748dbc842fc3370
Anmol Gupta <42043748+anmolg45@users.noreply.github.com>
2023-10-06 12:12:29 +0530

    basic js

--- App.js
+++
@@ -1,4 +0,0 @@
-const heading = React.createElement("h1", {}, "Testing abc");
-      const root = ReactDOM.createRoot(document.getElementById("root"));
-
-      root.render(heading);
---
+++ chapter -1 Inception/app.js
@@ -0,0 +1,5 @@
+const heading = document.createElement("h1");
+heading.innerHTML = "check this out";
+
+const root = document.getElementById("root");
+root.appendChild(heading);
