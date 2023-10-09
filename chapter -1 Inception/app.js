c6d090f2f3a613c02b4af1c8a030b1f435344439
Anmol Gupta <42043748+anmolg45@users.noreply.github.com>
2023-10-09 17:22:26 +0530

    1st lesson finish

--- chapter -1 Inception/app.js
+++ chapter -1 Inception/app.js
@@ -1,5 +1,14 @@
-const heading = document.createElement("h1");
-heading.innerHTML = "check this out";
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h2", {  }, "testing React dfd Anm "),
+    React.createElement("h2", {  }, "oiokay React dfd Anm "),
+  ])
+);
 
-const root = document.getElementById("root");
-root.appendChild(heading);
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
